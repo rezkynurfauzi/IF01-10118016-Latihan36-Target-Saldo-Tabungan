17cade67f7705254c8695ac99a977604dd618850
Rezky Nur Fauzi <55779378+rezkynurfauzi@users.noreply.github.com>
2019-10-17 14:22:07 +0700

    Add files via upload

---
+++ IF01-10118016-Latihan36-Target Saldo Tabungan/src/if01/pkg10118016/latihan36/target/saldo/tabungan/Target.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if01.pkg10118016.latihan36.target.saldo.tabungan;
+
+/**
+ *
+ * @author Rezky
+ */
+public class Target {
+    public double saldoAwal;
+    public float bunga;
+    public double saldoAkhir;
+
+
+    public void main(String args) {
+	throw new UnsupportedOperationException();
+	}
+
+    public void hitungSaldo() {
+	throw new UnsupportedOperationException();
+	}
+}
